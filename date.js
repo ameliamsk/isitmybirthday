51557e65f33ed656379df24d57e581a94a334c65
ameliamsk <almondchildmel@gmail.com>
2016-07-22 13:33:56 -0400

    Add files via upload

---
+++ date.js
@@ -0,0 +1,12 @@
+var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+
+if (month === 2 && day === 26)
+{
+    document.write("YES :\')")
+}
+else
+{
+    document.write("NO :\'(")
+}
