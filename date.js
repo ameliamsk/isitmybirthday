c2d909f6ef289b35c7dbb872325037c6c38d5f3e
ameliamsk <almondchildmel@gmail.com>
2016-07-22 14:32:41 -0400

    Update date.js

--- date.js
+++ date.js
@@ -1,12 +1,24 @@
-var currentTime = new Date()
-var month = currentTime.getMonth() + 1
-var day = currentTime.getDate()
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+canvas.width = 500;
+canvas.height = 259;
+
+var currentTime = new Date();
+var month = currentTime.getMonth() + 1;
+var day = currentTime.getDate();
 
 if (month === 2 && day === 26)
 {
-    document.write("YES :\')")
+    document.write("YES :\')");
 }
 else
 {
-    document.write("NO :\'(")
+    var img = new Image();
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0, 500, 259, 0, 0, 500, 259);
+    };
+    img.src = "Hillary-Duff.gif";
+    document.write("NO :\'(");
 }
